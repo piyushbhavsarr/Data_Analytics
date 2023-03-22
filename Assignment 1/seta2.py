0241d12c61489a687b886e0917e9e9ce248b326e
Piyush Bhavsar <114468009+piyushbhavsarr@users.noreply.github.com>
2023-03-22 12:04:59 +0530

    Update seta2.py

--- Assignment 1/seta2.py
+++ Assignment 1/seta2.py
@@ -1,40 +1,29 @@
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
-# Create 'realestate' data set with random data
-np.random.seed(42)
-n = 500
-ids = np.arange(1, n+1)
-flats = np.random.randint(1, 5, n)
-houses = np.random.randint(1, 6, n)
-purchases = 10_000 + flats * 5_000 + houses * 10_000 + np.random.normal(0, 5_000, n)
-realestate_df = pd.DataFrame({'ID': ids, 'flat': flats, 'houses': houses, 'purchases': purchases})
+# Load the dataset
+df = pd.read_csv('real_estate_dataset.csv')
 
-# Split data into independent and target variables
-X = realestate_df[['flat', 'houses']]
-y = realestate_df['purchases']
+# Split the dataset into input variables (X) and target variable (y)
+X = df[['house_age', 'distance_to_mrt']]
+y = df['house_price']
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# Split the dataset into a training set and a testing set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Print training and testing sets
-print("Training set:")
-print(X_train.head())
-print(y_train.head())
-print("\nTesting set:")
-print(X_test.head())
-print(y_test.head())
+# Create an instance of the LinearRegression class
+model = LinearRegression()
 
-# Build linear regression model
-lr_model = LinearRegression()
-lr_model.fit(X_train, y_train)
-y_pred = lr_model.predict(X_test)
-print("Linear regression model score:", lr_model.score(X_test, y_test))
+# Train the model on the training set
+model.fit(X_train, y_train)
 
-# Build simple linear regression model for predicting purchases
-slr_model = LinearRegression()
-slr_model.fit(X_train[['flat']], y_train)
-y_pred_slr = slr_model.predict(X_test[['flat']])
-print("Simple linear regression model score:", slr_model.score(X_test[['flat']], y_test))
+# Use the trained model to make predictions on the testing set
+y_pred = model.predict(X_test)
+
+# Calculate the mean squared error
+mse = mean_squared_error(y_test, y_pred)
+
+# Print the mean squared error
+print('Mean squared error: ', mse)
