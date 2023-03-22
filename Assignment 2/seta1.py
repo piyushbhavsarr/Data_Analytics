4286b934ebd504cf72fb028c2ec7e8f7048b1a72
Piyush Bhavsar <114468009+piyushbhavsarr@users.noreply.github.com>
2023-03-22 12:23:18 +0530

    bread,milk,diaper,beer,coke

---
+++ Assignment 2/seta1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+transactions=[['Bread'],['Milk'],
+              ['Bread'],['Diaper'],['Beer'],['Eggs'],
+              ['Milk'],['Diaper'],['Beer'],['Coke'],
+              ['Bread'],['Milk'],['Diaper'],['Beer'],
+              ['Bread'],['Milk'],['Diaper'],['Coke']]
+
+te=TransactionEncoder()
+te_array=te.fit(transactions).transform(transactions)
+df=pd.DataFrame(te_array,columns=te.columns_)
+
+print(df)
+
+freq_items=apriori(df,min_support=0.1, use_colnames=True)
+print(freq_items)
+
+rules=association_rules(freq_items,metric='support',min_threshold=0.01)
+rules=rules.sort_values(['support','confidence'],ascending=[False,False])
+print(rules)
